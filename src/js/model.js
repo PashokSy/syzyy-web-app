c6ce2745ad102abc1be6a8cf71e63d73ba428c38
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-12-07 21:05:42 +0200

    fix: model.getAllArticles

--- src/js/model.js
+++ src/js/model.js
@@ -5,24 +5,11 @@ export const state = {
   articles: [],
 };
 
-const getAuthorNickName = async function (id) {
-  try {
-    return nickName;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 export const getAllArticles = async function () {
   try {
     const articles = await AJAX(`${API_URL}article`);
 
-    await articles.forEach(async article => {
-      const { nickName } = await AJAX(`${API_URL}author/${article.authorId}`);
-      article.author = nickName;
-    });
-
-    state.articles.push(...articles);
+    state.articles = articles;
   } catch (error) {
     console.error(error);
   }
